ef4383fa888eb810e853a96e66cc38fd26218fe1
voditelkatka <voditelkatka@gmail.com>
2021-03-23 14:15:42 +0300

    feat: finish dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,20 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(names) {
-  var cleanNames = [];
-  var firstLetter = [];
-  
-  for (let i = 0; i < names.length; i++) {
-    if (typeof names[i] == "string") {
-      cleanNames.push(names[i]);
-    }
-    else {
-      return false;
-    }
-  }
-  
-  for (let i = 0; i < cleanNames.length; i++) {  
-    return firstLetter.push(cleanNames[i].slice(0, 1).toUpperCase());
-  }
-};
+var cleanNames = [];
+var firstLetter = [];
 
+if (names == null) {
+  return false;
+}
+
+for (let i = 0; i < names.length; i++) {
+  if (typeof names[i] == "string") {
+    cleanNames.push(names[i].trim());
+  }
+}
+
+for (let i = 0; i < cleanNames.length; i++) {  
+  firstLetter.push(cleanNames[i].slice(0, 1).toUpperCase());
+}
+
+return firstLetter.sort().join('');
+};
