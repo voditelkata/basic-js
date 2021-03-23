d0bab0d4ea68e73fd20979c1703ec4b7ae51e1fe
voditelkatka <voditelkatka@gmail.com>
2021-03-23 12:50:16 +0300

    feat: start dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(names) {
+  var cleanNames = [];
+  var firstLetter = [];
+  
+  for (let i = 0; i < names.length; i++) {
+    if (typeof names[i] == "string") {
+      cleanNames.push(names[i]);
+    }
+    else {
+      return false;
+    }
+  }
+  
+  for (let i = 0; i < cleanNames.length; i++) {  
+    return firstLetter.push(cleanNames[i].slice(0, 1).toUpperCase());
+  }
 };
+
