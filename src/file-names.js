8cf73eff94597f8486c1e886437793e8a8fae9f7
voditelkatka <voditelkatka@gmail.com>
2022-05-02 22:48:47 +0300

    feat: add mac-address solution

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let result = [];
+  names.forEach((item) => {
+    if (result.includes(item)) {
+      let count = 0;
+      for (let i = 0; i < names.length; i++) {
+        if (result[i] == item) {
+          count++;
+        }
+      }
+      result.push(`${item}(${count})`);
+      count = 0;
+    } else {
+      result.push(item);
+    }
+  });
+  return result;
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,10 +14,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let correct = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  let test = n.split("-").join("").split("");
+  return test.every((item) => correct.includes(item));
 }
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
